165331070695752afeb5bd19bc6cc8a6c4fb94be
Valentine Wallace <vwallace@protonmail.com>
2018-11-29 22:53:07 +0000

    Adapt Seed Verify screen for mobile.
    Additionally, enable enter-ing through verification words on mobile + desktop.

--- src/view/seed-verify.js
+++ src/view/seed-verify.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, KeyboardAvoidingView } from 'react-native';
+import { createStyles, maxWidth } from '../component/media-query';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import SeedEntry from '../component/seed-entry';
 import { Button, BackButton, GlasButton } from '../component/button';
-import { H1Text } from '../component/text';
 import { FormSubText } from '../component/form';
 import Background from '../component/background';
 import MainContent from '../component/main-content';
+import { CopyOnboardText } from '../component/text';
 import { Header } from '../component/header';
 import Card from '../component/card';
+import { breakWidth } from '../component/style';
 
 //
 // Seed Verify View
 //
 
-const styles = StyleSheet.create({
+const baseStyles = {
+  contentWrapper: {
+    flex: 1,
+  },
   content: {
     justifyContent: 'flex-end',
   },
@@ -30,35 +35,68 @@ const styles = StyleSheet.create({
     paddingRight: 45,
     paddingBottom: 50,
   },
-});
+};
 
-const SeedVerifyView = ({ store, nav, wallet }) => (
-  <Background image="purple-gradient-bg">
-    <Header>
-      <BackButton onPress={() => nav.goSeed()} />
-      <Button disabled onPress={() => {}} />
-    </Header>
-    <MainContent style={styles.content}>
-      <View>
-        <H1Text style={styles.title}>{"Let's double check"}</H1Text>
-      </View>
-      <Card style={styles.card}>
-        <FormSubText>{store.seedVerifyCopy}</FormSubText>
-        {store.seedVerifyIndexes.map((seedIndex, i) => (
-          <SeedEntry
-            seedIndex={seedIndex}
-            value={store.wallet.seedVerify[i]}
-            onChangeText={word => wallet.setSeedVerify({ word, index: i })}
-            key={i}
-            autoFocus={i === 0}
-            onSubmitEditing={() => wallet.checkSeed()}
-          />
-        ))}
-      </Card>
-      <GlasButton onPress={() => wallet.checkSeed()}>Next</GlasButton>
-    </MainContent>
-  </Background>
+const styles = createStyles(
+  baseStyles,
+
+  maxWidth(breakWidth, {
+    card: {
+      width: undefined,
+    },
+  })
 );
+
+class SeedVerifyView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focusedInput: 0,
+    };
+  }
+
+  render() {
+    const { store, nav, wallet } = this.props;
+    return (
+      <Background image="purple-gradient-bg">
+        <Header>
+          <BackButton onPress={() => nav.goSeed()} />
+          <Button disabled onPress={() => {}} />
+        </Header>
+        <KeyboardAvoidingView behavior="padding" style={styles.contentWrapper}>
+          <MainContent style={styles.content}>
+            <View>
+              <CopyOnboardText style={styles.title}>
+                {"Let's double check"}
+              </CopyOnboardText>
+            </View>
+            <Card style={styles.card}>
+              <FormSubText>{store.seedVerifyCopy}</FormSubText>
+              {store.seedVerifyIndexes.map((seedIndex, i) => (
+                <SeedEntry
+                  seedIndex={seedIndex}
+                  value={store.wallet.seedVerify[i]}
+                  onChangeText={word =>
+                    wallet.setSeedVerify({ word, index: i })
+                  }
+                  key={i}
+                  autoFocus={i === this.state.focusedInput}
+                  onSubmitEditing={() =>
+                    i === 2
+                      ? wallet.checkSeed()
+                      : this.setState({ focusedInput: i + 1 })
+                  }
+                  onClick={() => this.setState({ focusedInput: i })}
+                />
+              ))}
+            </Card>
+            <GlasButton onPress={() => wallet.checkSeed()}>Next</GlasButton>
+          </MainContent>
+        </KeyboardAvoidingView>
+      </Background>
+    );
+  }
+}
 
 SeedVerifyView.propTypes = {
   store: PropTypes.object.isRequired,
