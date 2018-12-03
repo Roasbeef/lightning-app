346ecef84b1a13ca448a5e18321fdd9ae09feba2
Valentine Wallace <vwallace@protonmail.com>
2018-12-03 22:13:32 +0000

    Implement Invoice for mobile.
    There's a lot of separate styling for large vs. smaller phone screens.

--- src/view/invoice.js
+++ src/view/invoice.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Dimensions } from 'react-native';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
+import { createStyles, maxWidth } from '../component/media-query';
 import Background from '../component/background';
 import MainContent from '../component/main-content';
 import { InputField, AmountInputField } from '../component/field';
@@ -11,9 +12,16 @@ import { BalanceLabel, BalanceLabelUnit } from '../component/label';
 import Card from '../component/card';
 import LightningBoltIcon from '../asset/icon/lightning-bolt';
 import { FormStretcher, FormSubText } from '../component/form';
-import { color } from '../component/style';
+import { color, font, breakWidth, smallBreakWidth } from '../component/style';
 
-const styles = StyleSheet.create({
+const baseStyles = {
+  contentWrapper: {
+    flex: 1,
+  },
+  card: {
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
   balance: {
     marginTop: 40,
   },
@@ -24,7 +32,61 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     paddingBottom: 40,
   },
-});
+  amountInput: {},
+  form: {},
+  input: {},
+  nextBtn: {},
+};
+
+const styles = createStyles(
+  baseStyles,
+
+  maxWidth(breakWidth, {
+    balance: {
+      marginTop: 0,
+    },
+    amountInput: {
+      lineHeight: 95,
+    },
+    unit: {
+      lineHeight: font.lineHeightXXXL,
+      paddingRight: 20,
+      paddingBottom: 30,
+    },
+    input: {
+      lineHeight: font.lineHeightM + 5,
+    },
+    subText: {
+      paddingLeft: 25,
+      paddingRight: 25,
+    },
+  }),
+
+  maxWidth(smallBreakWidth, {
+    amountInput: {
+      lineHeight: 45,
+      fontSize: font.sizeXXL,
+    },
+    unit: {
+      marginLeft: 5,
+      paddingRight: 25,
+      paddingBottom: 10,
+    },
+    form: {
+      paddingTop: 0,
+      paddingBottom: 0,
+    },
+    subText: {
+      width: 300,
+      paddingTop: 10,
+      paddingBottom: 10,
+    },
+    nextBtn: {
+      alignSelf: 'center',
+      width: 300,
+    },
+  })
+);
 
 const InvoiceView = ({ store, nav, invoice }) => (
   <Background image="purple-gradient-bg">
@@ -35,34 +97,46 @@ const InvoiceView = ({ store, nav, invoice }) => (
       </Title>
       <CancelButton onPress={() => nav.goHome()} />
     </Header>
-    <MainContent>
-      <Card>
-        <BalanceLabel style={styles.balance}>
-          <AmountInputField
-            autoFocus={true}
-            value={store.invoice.amount}
-            onChangeText={amount => invoice.setAmount({ amount })}
-            onSubmitEditing={() => invoice.generateUri()}
-          />
-          <BalanceLabelUnit style={styles.unit}>
-            {store.unitFiatLabel}
-          </BalanceLabelUnit>
-        </BalanceLabel>
-        <FormStretcher>
-          <InputField
-            placeholder="Note"
-            value={store.invoice.note}
-            onChangeText={note => invoice.setNote({ note })}
-            onSubmitEditing={() => invoice.generateUri()}
-          />
-        </FormStretcher>
-        <FormSubText style={styles.subText}>
-          Generate a payment request that others can use to pay you immediately
-          via the Lightning Network.
-        </FormSubText>
-        <PillButton onPress={() => invoice.generateUri()}>Next</PillButton>
-      </Card>
-    </MainContent>
+    <KeyboardAvoidingView style={styles.contentWrapper} behavior="padding">
+      <MainContent>
+        <Card style={styles.card}>
+          <BalanceLabel style={styles.balance}>
+            <AmountInputField
+              style={styles.amountInput}
+              charWidth={
+                Dimensions.get('window').width < smallBreakWidth ? 26 : 46
+              }
+              autoFocus={true}
+              value={store.invoice.amount}
+              onChangeText={amount => invoice.setAmount({ amount })}
+              onSubmitEditing={() => invoice.generateUri()}
+            />
+            <BalanceLabelUnit style={styles.unit}>
+              {store.unitFiatLabel}
+            </BalanceLabelUnit>
+          </BalanceLabel>
+          <FormStretcher style={styles.form}>
+            <InputField
+              style={styles.input}
+              placeholder="Note"
+              value={store.invoice.note}
+              onChangeText={note => invoice.setNote({ note })}
+              onSubmitEditing={() => invoice.generateUri()}
+            />
+          </FormStretcher>
+          <FormSubText style={styles.subText}>
+            Generate a payment request that others can use to pay you
+            immediately via the Lightning Network.
+          </FormSubText>
+          <PillButton
+            style={styles.nextBtn}
+            onPress={() => invoice.generateUri()}
+          >
+            Next
+          </PillButton>
+        </Card>
+      </MainContent>
+    </KeyboardAvoidingView>
   </Background>
 );
 
